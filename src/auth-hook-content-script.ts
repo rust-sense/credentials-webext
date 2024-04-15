a6afc21c7b0c8acb190238de162938f241116d2b
Jaime Martínez Rincón <jaime@jamezrin.name>
2024-04-15 23:48:45 +0200

    chore: add proto files

--- src/auth-hook-content-script.ts
+++ src/auth-hook-content-script.ts
@@ -1,18 +1,18 @@
-console.debug("Registering ReactNativeWebView.postMessage handler");
+console.debug("Registering ReactNativeWebView.postMessage handler")
 
 // biome-ignore lint/suspicious/noExplicitAny: Needed to define ReactNativeWebView in window
-(window as any).ReactNativeWebView = {
-	postMessage: (message) => {
-		console.debug(
-			"Received rust companion authentication data, forwarding it to the extension...",
-		);
+;(window as any).ReactNativeWebView = {
+  postMessage: (message) => {
+    console.debug(
+      "Received rust companion authentication data, forwarding it to the extension...",
+    )
 
-		const authData = JSON.parse(message);
-		window.postMessage({
-			rustCompanionAuth: {
-				steamId: authData.SteamId,
-				token: authData.Token,
-			},
-		});
-	},
-};
+    const authData = JSON.parse(message)
+    window.postMessage({
+      rustCompanionAuth: {
+        steamId: authData.SteamId,
+        token: authData.Token,
+      },
+    })
+  },
+}
--- src/background.ts
+++ src/background.ts
@@ -1,75 +1,75 @@
 async function getExpoPushToken(
-	deviceId: string,
-	registrationId: string,
+  deviceId: string,
+  registrationId: string,
 ): Promise<string> {
-	const requestBody = JSON.stringify({
-		deviceId: deviceId,
-		experienceId: "@facepunch/RustCompanion",
-		appId: "com.facepunch.rust.companion",
-		deviceToken: registrationId,
-		type: "fcm",
-		development: false,
-	});
+  const requestBody = JSON.stringify({
+    deviceId: deviceId,
+    experienceId: "@facepunch/RustCompanion",
+    appId: "com.facepunch.rust.companion",
+    deviceToken: registrationId,
+    type: "fcm",
+    development: false,
+  })
 
-	const response = await fetch(
-		"https://exp.host/--/api/v2/push/getExpoPushToken",
-		{
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: requestBody,
-		},
-	);
+  const response = await fetch(
+    "https://exp.host/--/api/v2/push/getExpoPushToken",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: requestBody,
+    },
+  )
 
-	const jsonResponse = await response.json();
-	return jsonResponse.data.expoPushToken;
+  const jsonResponse = await response.json()
+  return jsonResponse.data.expoPushToken
 }
 
 async function registerWithRustPlus(authToken: string, expoPushToken: string) {
-	const requestBody = JSON.stringify({
-		AuthToken: authToken,
-		DeviceId: "rustplus.js",
-		PushKind: 0,
-		PushToken: expoPushToken,
-	});
+  const requestBody = JSON.stringify({
+    AuthToken: authToken,
+    DeviceId: "rustplus.js",
+    PushKind: 0,
+    PushToken: expoPushToken,
+  })
 
-	const response = await fetch(
-		"https://companion-rust.facepunch.com:443/api/push/register",
-		{
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: requestBody,
-		},
-	);
+  const response = await fetch(
+    "https://companion-rust.facepunch.com:443/api/push/register",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: requestBody,
+    },
+  )
 
-	const jsonResponse = await response.json();
-	return jsonResponse;
+  const jsonResponse = await response.json()
+  return jsonResponse
 }
 
 type Registration = {
-	gcm: {
-		androidId: string;
-		securityToken: string;
-		appId: string;
-		token: string;
-	};
-	fcm: {
-		token: string;
-		pushSet: string;
-	};
-	keys: {
-		privateKey: string;
-		publicKey: string;
-		authSecret: string;
-	};
-};
+  gcm: {
+    androidId: string
+    securityToken: string
+    appId: string
+    token: string
+  }
+  fcm: {
+    token: string
+    pushSet: string
+  }
+  keys: {
+    privateKey: string
+    publicKey: string
+    authSecret: string
+  }
+}
 
 async function registerCredentials(authToken: string): Promise<Registration> {
-	//crypto.subtle.generateKey()
-	/*
+  //crypto.subtle.generateKey()
+  /*
   const rustAppSenderId = '976529667804';
 
   const deviceId = crypto.randomUUID();
@@ -81,36 +81,36 @@ async function registerCredentials(authToken: string): Promise<Registration> {
   console.log('Registered with Rust+', response);
   */
 
-	return {
-		gcm: {
-			androidId: "androidId",
-			securityToken: "security",
-			appId: "appId",
-			token: "token",
-		},
-		fcm: {
-			token: "token",
-			pushSet: "pushSet",
-		},
-		keys: {
-			privateKey: "privateKey",
-			publicKey: "publicKey",
-			authSecret: "authSecret",
-		},
-	};
+  return {
+    gcm: {
+      androidId: "androidId",
+      securityToken: "security",
+      appId: "appId",
+      token: "token",
+    },
+    fcm: {
+      token: "token",
+      pushSet: "pushSet",
+    },
+    keys: {
+      privateKey: "privateKey",
+      publicKey: "publicKey",
+      authSecret: "authSecret",
+    },
+  }
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	console.log("Received message:", request);
-	if (request.type === "REGISTER_FCM") {
-		const { token } = request;
-		registerCredentials(token).then((credentials) => {
-			console.log("Registered credentials:", credentials);
-			setTimeout(() => {
-				sendResponse(credentials);
-			}, 5000);
-		});
+  console.log("Received message:", request)
+  if (request.type === "REGISTER_FCM") {
+    const { token } = request
+    registerCredentials(token).then((credentials) => {
+      console.log("Registered credentials:", credentials)
+      setTimeout(() => {
+        sendResponse(credentials)
+      }, 5000)
+    })
 
-		return true;
-	}
-});
+    return true
+  }
+})
